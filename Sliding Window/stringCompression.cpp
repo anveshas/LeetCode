98e2af52fc0ee534ba26a087fbaceecf64f61dd1
Anvesha Srivastava <aardhya@Munmuns-MacBook-Air.local>
2024-08-08 20:14:38 +0530

    initial commit

---
+++ Sliding Window/stringCompression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int n = chars.size();
+
+        string ans = "";
+        while (i < n) {
+            char ch = chars[i];
+            int cnt = 0;
+            while (i < n && chars[i] == ch) {
+                cnt++;
+                i++;
+            }
+            ans += ch;
+
+            if (cnt > 1) {
+                ans += to_string(cnt);
+            }
+        }
+        chars.clear();
+        for (char ch : ans) {
+            chars.push_back(ch);
+        }
+
+        return ans.size();
+    }
+};
